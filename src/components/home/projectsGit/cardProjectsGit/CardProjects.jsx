49ed34801f4a071adf59c3a9fd49403150c8c429
Neampi <piero.neam@gmail.com>
2024-03-07 22:33:57 -0500

    Links

--- src/components/home/projectsGit/cardProjectsGit/CardProjects.jsx
+++ src/components/home/projectsGit/cardProjectsGit/CardProjects.jsx
@@ -17,7 +17,18 @@ const CardProjects = ({id, name, html_url, language}) => {
         <div class="content">
             <div class="avatar"><img src="https://avatars.githubusercontent.com/u/95370447?v=4" alt="" /></div>
             <div class="content__text">
-                <span class="stream__title">🚀<a href={html_url}>{name}</a></span>
+                <span class="stream__title">🚀
+                {name === 'APP_CLIMA_JS' ? <a href="https://clima-maps.netlify.app/">{name}</a> : 
+                name === 'landing-cloud-react' ? <a href="https://landing-cloud.netlify.app/">{name}</a> : 
+                name === 'challenge_home_react' ? <a href="https://challenge-home-react.netlify.app/">{name}</a> : 
+                name === 'React_page_RentUp' ? <a href="https://rentuppy.netlify.app/">{name}</a> : 
+                name === 'GENERATE-GIFTS-2.0' ? <a href="https://generate-gift.netlify.app/">{name}</a> :
+                name === 'landing-mobile-react' ? <a href="https://appdownloadprueba.netlify.app/">{name}</a> : 
+                name === 'tic-tac-toe-game' ? <a href="https://michi-game.netlify.app/">{name}</a> :
+                name === 'Game-Oracle-One' ? <a href="https://game-oracle-one.netlify.app/">{name}</a> :
+                name === 'ppyecom.github.io' ? <a href="https://ppyecom.github.io/">Encriptador Alura</a> :
+                <a href={html_url}>{name}</a>}
+                </span>
                 <div class="content__body">
                     <span class="streamer__name">PYECOM</span>
                     <span class="event">{id}</span>
@@ -25,6 +36,7 @@ const CardProjects = ({id, name, html_url, language}) => {
                 <span class="categories">
                     
                   <a key={language} class="categories__btn" href="#">{language}</a>
+                  <a class="categories__btn" href="#">GitHub</a>
                         
                 </span>
             </div>
